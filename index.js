0c7247af952a11a81062f295e60597f6c56daa4e
WayneChang <mail.chewei@gmail.com>
2019-08-31 01:57:43 +0800

    use a 2d array to save shifts

--- index.js
+++ index.js
@@ -26,116 +26,141 @@ class ShiftPlan {
   constructor(monthSetting, employees) {
     this.monthSetting = monthSetting;
     this.employees = employees;
+    this.rowLength = monthSetting.days + Object.keys(ShiftType).length + 2;
+    this.colLength = employees.length + 1;
+    this.dayOffTable = this._initialDayOffTable(employees, this.rowLength, this.colLength);
+  }
+
+  _initialDayOffTable(employees, rowLength, colLength) {
+    const dayOffTable = Array.from(
+      Array(colLength),
+      () => Array(rowLength).fill(''));
+
+    employees.forEach(emp => {
+      dayOffTable[emp.index][0] = emp.name;
+      emp.planDayOffDates.forEach(date => {
+        this._setEmpoyeeShift(dayOffTable, emp, date, ShiftType.DayOff);
+      });
+    });
+
+    return dayOffTable;
   }
 
   start() {
-    for (let date = 1; date <= this.monthSetting.days; date++) {
-      const candidates = this.employees.filter(employee => {
-        if (!employee.isCandidate(date)) {
-          employee.setShift(date, ShiftType.DayOff);
-          return false;
+    this._preAddDayOff(this.dayOffTable, this.monthSetting, this.employees);
+  }
+
+  _preAddDayOff(dayOffTable, monthSetting, employees) {
+    let beginDateOfWeekOfMonth = 1 + (7 - monthSetting.beginDay + 1);
+    for (let beginDateOfWeek = beginDateOfWeekOfMonth; beginDateOfWeek < monthSetting.days; beginDateOfWeek += 7) {
+      employees.forEach(emp => {
+        let count = 0;
+        for (let day = 0; day < 3; day++) {
+          if (dayOffTable[emp.index][beginDateOfWeek + day] === ShiftType.DayOff) count++;
         }
-        return true;
-      });
-      const shiftTypes = this.monthSetting.getShifts(date);
-      shiftTypes.forEach(shiftType => {
-        let shiftTypeCandidates = candidates;
-        if (shiftType === ShiftType.FullDay || shiftType === ShiftType.NightShiftOvertime) {
-          shiftTypeCandidates = this.getLowestShiftTypeCountCandidates(shiftTypeCandidates, shiftType);
+        if (count >= 2 && beginDateOfWeek + 7 <= monthSetting.days) this._setEmpoyeeShift(dayOffTable, emp, beginDateOfWeek + 7, ShiftType.DayOff);
+        count = 0;
+        for (let day = 4; day < 7; day++) {
+          if (dayOffTable[emp.index][beginDateOfWeek + day] === ShiftType.DayOff) count++;
         }
-        const chosen = this.getRandomOneFromItems(shiftTypeCandidates);
-        chosen.setShift(date, shiftType);
-        candidates.splice(candidates.indexOf(chosen), 1);
-      });
-      candidates.forEach(candidate => {
-        candidate.setShift(date, ShiftType.DayOff);
+        if (count >= 2 && beginDateOfWeek - 1 > 0) this._setEmpoyeeShift(dayOffTable, emp, beginDateOfWeek - 1, ShiftType.DayOff);
       });
     }
   }
 
-  getLowestShiftTypeCountCandidates(candidates, shiftType) {
-    let result = [];
-    let minCount = Number.MAX_VALUE;
-    candidates.filter(c => c.role !== Role.MG).forEach(candidate => {
-      if (minCount > candidate.shiftTypeCount[shiftType]) {
-        result = [candidate];
-        minCount = candidate.shiftTypeCount[shiftType];
-      } else {
-        result.push(candidate);
-      }
-    });
-    return result;
-  }
-
-  getRandomOneFromItems(items) {
-    return items[Math.floor(Math.random() * items.length)];
+  _setEmpoyeeShift(dayOffTable, employee, date, shiftType) {
+    dayOffTable[employee.index][date] = shiftType;
   }
 
   printRoster() {
-    const tableRows = this.employees.map(employee => {
-      const row = { name: employee.name };
-      for (let date = 1; date <= this.monthSetting.days; date++) {
-        row[date] = employee.shifts[date - 1];
-      }
-      for (let shiftType in ShiftType) {
-        row[shiftType] = employee.shiftTypeCount[ShiftType[shiftType]];
-      }
-      row.role = employee.role;
-      return row;
-    });
+    this._refreshEveryDateDayOffCount(this.dayOffTable, this.colLength);
 
     const docFrag = new DocumentFragment();
-
-    this.populateRosterTableHeader(docFrag);
-
-    tableRows.forEach(row => {
-      const tr = document.createElement("tr");
-      const td = document.createElement("td");
-      td.innerText = row.name;
-      tr.appendChild(td);
-
-      for (let key in row) {
-        if (key === 'name') continue;
-        const td = document.createElement("td");
-        if (row.role === Role.MG && (key === 'NightShiftOvertime' || key === 'FullDay')) {
-          td.innerText = 'X';
-        } else {
-          td.innerText = row[key];
-        }
-        tr.appendChild(td);
-      }
-      docFrag.appendChild(tr);
-    });
-    const tableDom = document.querySelector("#roster");
-    tableDom.appendChild(docFrag);
+    this._populateRosterTableHeader(docFrag, this.monthSetting, this.rowLength);
+    this._populateRosterTableBody(docFrag, this.dayOffTable);
+    this._populateRosterTableFoot(docFrag, this.rowLength);
+    document.querySelector("#roster").appendChild(docFrag);
   }
 
-  populateRosterTableHeader(docFrag) {
+  _refreshEveryDateDayOffCount(dayOffTable, colLength) {
+    for (let date = 1; date <= this.monthSetting.days; date += 1) {
+      this._refreshDateDayOffCount(dayOffTable, date, colLength);
+    }
+  }
+
+  _refreshDateDayOffCount(dayOffTable, date, colLength) {
+    dayOffTable[colLength - 1][date] = 0;
+    for (let col = 0; col < colLength - 1; col += 1) {
+      if (dayOffTable[col][date] === ShiftType.DayOff) dayOffTable[colLength - 1][date] += 1;
+    }
+  }
+
+  _populateRosterTableHeader(docFrag, monthSetting, rowLength) {
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.innerText = "header";
+    td.colSpan = rowLength;
+    tr.appendChild(td);
+    thead.appendChild(tr);
+    this._populateRosterTableHeaderDate(thead, monthSetting.days);
+    this._populateRosterTableHeaderDay(thead, monthSetting);
+    docFrag.appendChild(thead, this.rowLength);
+  }
+
+  _populateRosterTableHeaderDate(thead, days) {
     const dateTr = document.createElement("tr");
-    const dayTr = document.createElement("tr");
     dateTr.appendChild(document.createElement("th"));
-    dayTr.appendChild(document.createElement("th"));
-    for (let date = 1; date <= this.monthSetting.days; date++) {
+    for (let date = 1; date <= days; date++) {
       const dateTh = document.createElement("th");
       dateTh.innerText = date;
       dateTr.appendChild(dateTh);
+    }
+    thead.appendChild(dateTr);
+  }
 
+  _populateRosterTableHeaderDay(thead, monthSetting) {
+    const dayTr = document.createElement("tr");
+    dayTr.appendChild(document.createElement("th"));
+    for (let date = 1; date <= monthSetting.days; date++) {
       const dayTh = document.createElement("th");
-      dayTh.innerText = this.monthSetting.getDay(date);
+      dayTh.innerText = monthSetting.getDay(date);
       dayTr.appendChild(dayTh);
     }
     for (let shiftType in ShiftType) {
       const th = document.createElement("th");
       th.innerText = ShiftType[shiftType];
       dayTr.appendChild(th);
-      dateTr.appendChild(document.createElement("th"));
     }
     const th = document.createElement("th");
     th.innerText = "職";
     dayTr.appendChild(th);
-    dateTr.appendChild(document.createElement("th"));
-    docFrag.appendChild(dateTr);
-    docFrag.appendChild(dayTr);
+    thead.appendChild(dayTr);
+  }
+
+  _populateRosterTableBody(docFrag, tableRows) {
+    const tbody = document.createElement('tbody');
+    tableRows.forEach(row => {
+      const tr = document.createElement("tr");
+      row.forEach(cell => {
+        const td = document.createElement("td");
+        td.innerText = cell;
+        tr.appendChild(td);
+      });
+      tbody.appendChild(tr);
+    });
+    docFrag.appendChild(tbody);
+  }
+
+  _populateRosterTableFoot(docFrag, rowLength) {
+    const tfoot = document.createElement('tfoot');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.innerText = "footer";
+    td.colSpan = rowLength;
+    tr.appendChild(td);
+    tfoot.appendChild(tr);
+    docFrag.appendChild(tfoot);
   }
 }
 
@@ -167,7 +192,8 @@ class MonthSetting {
 }
 
 class Employee {
-  constructor({ name, planDayOffDates, role, maxSeqShift = 4 }) {
+  constructor({ index, name, planDayOffDates, role, maxSeqShift = 4 }) {
+    this.index = index;
     this.name = name;
     this.planDayOffDates = planDayOffDates;
     this.role = role;
