1582b52c7a59acd0b822a609c8b8189dcbf9e15e
WayneChang <mail.chewei@gmail.com>
2019-09-09 23:51:13 +0800

    start using react

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div id="step1" className="step">test</div>
+      <div id="step2" className="step">test</div>
+      <div id="step3" className="step">test</div>
+    </div>
+  );
+}
+
+export default App;
