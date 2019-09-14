9eb3c55ddc5d42a357dbfa748cda3fa39034b753
WayneChang <mail.chewei@gmail.com>
2019-09-14 16:29:50 +0800

    add css for date and day

--- src/components/plan-day-off-table/date-row.jsx
+++ src/components/plan-day-off-table/date-row.jsx
@@ -8,16 +8,28 @@ export default class DateRow extends PureComponent {
     dateHeaders.push(<th key="pre padding" />);
     const firstDateOfTable = 1 + monthData.lastMonthDays - monthData.lastMonthLastWeekDays;
     for (let date = firstDateOfTable; date <= monthData.lastMonthDays; date += 1) {
-      dateHeaders.push(<th key={`last${date}`}>{date}</th>);
+      if (date === monthData.lastMonthDays) {
+        dateHeaders.push(<th key={`last${date}`} className="endOfLastMonth">{date}</th>);
+      } else {
+        dateHeaders.push(<th key={`last${date}`}>{date}</th>);
+      }
     }
     for (let date = 1; date <= monthData.days; date += 1) {
-      dateHeaders.push(<th key={`this${date}`}>{date}</th>);
+      if (date === monthData.days) {
+        dateHeaders.push(<th key={`this${date}`} className="endOfThisMonth">{date}</th>);
+      } else {
+        dateHeaders.push(<th key={`this${date}`}>{date}</th>);
+      }
     }
     for (let date = 1; date <= monthData.nextMonthFirstWeekDays; date += 1) {
-      dateHeaders.push(<th key={`next${date}`}>{date}</th>);
+      if (date === monthData.nextMonthFirstWeekDays) {
+        dateHeaders.push(<th key={`next${date}`} className="endOfNextMonth">{date}</th>);
+      } else {
+        dateHeaders.push(<th key={`next${date}`}>{date}</th>);
+      }
     }
     return (
-      <tr>
+      <tr className="date-row">
         {dateHeaders}
       </tr>
     );
--- src/components/plan-day-off-table/day-row.jsx
+++ src/components/plan-day-off-table/day-row.jsx
@@ -9,18 +9,30 @@ export default class DayRow extends PureComponent {
     const firstDateOfTable = 1 + monthData.lastMonthDays - monthData.lastMonthLastWeekDays;
     for (let date = firstDateOfTable; date <= monthData.lastMonthDays; date += 1) {
       const day = monthData.getDay(date - monthData.lastMonthDays);
-      dayHeaders.push(<th key={`last${date} ${day}`}>{day}</th>);
+      if (date === monthData.lastMonthDays) {
+        dayHeaders.push(<th key={`last${date} ${day}`} className="endOfLastMonth">{day}</th>);
+      } else {
+        dayHeaders.push(<th key={`last${date} ${day}`}>{day}</th>);
+      }
     }
     for (let date = 1; date <= monthData.days; date += 1) {
       const day = monthData.getDay(date);
-      dayHeaders.push(<th key={`this${date} ${day}`}>{day}</th>);
+      if (date === monthData.days) {
+        dayHeaders.push(<th key={`this${date} ${day}`} className="endOfThisMonth">{day}</th>);
+      } else {
+        dayHeaders.push(<th key={`this${date} ${day}`}>{day}</th>);
+      }
     }
     for (let date = 1; date <= monthData.nextMonthFirstWeekDays; date += 1) {
       const day = monthData.getDay(monthData.days + date);
-      dayHeaders.push(<th key={`next${date} ${day}`}>{day}</th>);
+      if (date === monthData.nextMonthFirstWeekDays) {
+        dayHeaders.push(<th key={`next${date} ${day}`} className="endOfNextMonth">{day}</th>);
+      } else {
+        dayHeaders.push(<th key={`next${date} ${day}`}>{day}</th>);
+      }
     }
     return (
-      <tr>
+      <tr className="day-row">
         {dayHeaders}
       </tr>
     );
