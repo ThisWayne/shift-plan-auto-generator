2e33e268390159af8bea93dbc5c75284d5b630a6
WayneChang <mail.chewei@gmail.com>
2019-09-21 00:55:53 +0800

    Updates

---
+++ precache-manifest.334d64caf723eaa1c27e80dd7756642b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7434183fe90b8d710abe12fdf21159a9",
+    "url": "/ThisWayne/shift-plan-auto-generator/index.html"
+  },
+  {
+    "revision": "306844a296a82d9568c9",
+    "url": "/ThisWayne/shift-plan-auto-generator/static/css/main.c2bad016.chunk.css"
+  },
+  {
+    "revision": "8d43822b35c3f63ab062",
+    "url": "/ThisWayne/shift-plan-auto-generator/static/js/2.614eb4a8.chunk.js"
+  },
+  {
+    "revision": "306844a296a82d9568c9",
+    "url": "/ThisWayne/shift-plan-auto-generator/static/js/main.c6b05612.chunk.js"
+  },
+  {
+    "revision": "28d7b2f744fc095a18c7",
+    "url": "/ThisWayne/shift-plan-auto-generator/static/js/runtime~main.e4d8b7c3.js"
+  }
+]);
